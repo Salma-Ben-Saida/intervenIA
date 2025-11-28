c72c0a43cb8fab05e299f822921bdc32f3904354
khaoula <saidasalma83@gmail.com>
2025-11-28 21:57:24 +0100

    Project initiation

---
+++ src/main/java/tn/intervent360/intervent360/Intervent360Application.java
@@ -0,0 +1,13 @@
+package tn.intervent360.intervent360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Intervent360Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Intervent360Application.class, args);
+    }
+
+}
